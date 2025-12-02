da9aa25c4dc4ea2f8382cd5b676b2ca7e75aa9eb
RZakrzewski02 <rafalzakrzewskitzn@gmail.com>
2025-12-02 18:33:37 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 3000;
 
@@ -12,7 +15,7 @@ mongoose.connect(process.env.MONGO_URI)
   .catch(err => console.error(err));
 
 app.get("/", (req, res) => {
-  res.send("API działa ✔️");
+  res.sendFile(path.join(__dirname, 'views', 'get_started.html'));
 });
 
 app.listen(PORT, () => console.log("Server running on port 3000"));
