3f376613d3a109738151c4ac966c1a0c27533bfa
RZakrzewski02 <rafalzakrzewskitzn@gmail.com>
2025-12-02 17:48:47 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error(err));
+
+app.get("/", (req, res) => {
+  res.send("API działa ✔️");
+});
+
+app.listen(PORT, () => console.log("Server running on port 3000"));
